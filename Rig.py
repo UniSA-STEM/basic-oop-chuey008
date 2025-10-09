58c37ec270ab8d33982deeaf3b9ba9590e440a73
Me <Me@me>
2025-10-09 18:20:32 +1030

    #Header - author details
    Updated the header to include
    author details

--- Rig.py
+++ Rig.py
@@ -7,9 +7,10 @@ Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
-#File: Rig.py
-#Description: The rig is a computer object that has a name, damage counter (starting at 0), a broken state (False) and a storage for assets.
-#Author: Emily Chuong
-#ID: 110448094
-#Username: Chuey008
-#This is my own work as defined by the University's Academic Misconduct Policy.
+# File: Rig.py
+# Description: The rig is a computer object that has a name, damage counter (starting at 0), a broken state (False) and a storage for assets.
+# Author: Emily Chuong
+# ID: 110448094
+# Username: Chuey008
+# This is my own work as defined by the University's Academic Misconduct Policy.
+
