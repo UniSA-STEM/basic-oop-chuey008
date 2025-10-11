4ee913335b609c46240ae66ac60ce6602bf875c2
Me <Me@me>
2025-10-11 21:07:51 +1030

    #Header & the main parts of Asset
    Added the Author Details in the header
    Created the class for Asset with
    Name, Description and Encryption
    and Decryption Boolean

--- Asset.py
+++ Asset.py
@@ -6,3 +6,27 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+# File: Asset.py
+# Description: The class represents a digital Asset object that has a name, description, a new Boolean attribute that is encrypted.
+# Author: Emily Chuong
+# ID: 110448094
+# Username: Chuey008
+# This is my own work as defined by the University's Academic Misconduct Policy.
+
+class Asset:
+    def __init__(self, name: str, description: str, encrypted: bool = False):
+        self.name = name
+        self.description = description
+        self.encrypted = encrypted
+
+    def encrypt(self):
+        self.encrypted = True
+
+    def decrypt(self):
+        self.encrypted = False
+
+    def __str__(self):
+        s = f"{self.name}: {self.description}"
+        return f"{s} [Encrypted:" if self.encrypted else s
+
