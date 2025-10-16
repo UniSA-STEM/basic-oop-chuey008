806d3d9b37a9f243ade08d94e530290a2e7ba2af
Me <Me@me>
2025-10-16 18:04:05 +1030

    # Add a storage in Rig
    Added a storage for the
    assets to sit in rig
    and release code if not
    encrypted vs encrypted

--- Rig.py
+++ Rig.py
@@ -10,12 +10,26 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 # from asset import Assets, DataSpike, RemovableDrive, CryptoToken, HardwarePatch, SecurityChip
 
 class Rig:
+    """
+    Creating a rig that contains the below attributes.
+    """
     def __init__(self, name: str):
-        self.name = name
-        self.damage = 0
-        self.broken = False
-        self.upgrade_level = 0
-        self.storage = [DataSpike(), DataSpike(), RemovableDrive()]
+        self._name = name
+        self._damage = 0
+        self._broken = False
+        self._upgrade_level = 0
+        # starting with 2 DataSpikes and 1 RemovableDrive
+        self._storage = [DataSpike(), DataSpike(), RemovableDrive()]
+
+    # creating a storage for the asset to be stored and extracted from
+    def store_asset(self, asset): self._storage.append(asset)
+
+    def release_asset_by_name(self, n):
+        for i, a in enumerate(self._storage):
+            if getattr(a, "name", "") == n:  # using getattr to safely access the asset name
+                if getattr(a, "encrypted", False): return None  # if it is encrypted it will not release
+                return self._storage.pop(i)  # if the asset matches and is not encrypted -> removed and returned
+        return None
 
     def take_hit(self):
         effective_threshold = 2 + self.upgrade_level
