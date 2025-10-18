c75984cfc23a8632323451ff92f9b52ea701d013
Me <Me@me>
2025-10-18 21:40:30 +1030

    # testing hacker
    Added hacker name

--- Main.py
+++ Main.py
@@ -7,19 +7,30 @@ Username: <Chuey008>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
-from Asset import Asset, CryptoToken, DataSpike, RemovableDrive, SecurityChip, HardwarePatch
-from Rig import Rig
 from Hacker import Hacker
+from Asset import DataSpike, CryptoToken, RemovableDrive, SecurityChip, HardwarePatch
+from Rig import Rig
 
 def main():
     # Create a hacker
     hacker = Hacker("Fidelius")
     print(hacker)
 
-    # Acquire a rig
-    print("\n-- Acquiring Rig --")
-    hacker.acquire_rig()
+def test_acquire_and_upgrade():
+    hacker = Hacker("Fidelius")
     print(hacker)
+
+    hacker.upgrade_rig()
+
+    # Acquire rig
+    hacker.add_to_inventory(CryptoToken())
+    hacker.acquire_rig()
+
+    # Upgrade rig
+    hacker.add_to_inventory(HardwarePatch())
+    hacker.upgrade_rig()
+
+    print(hacker.get_rig())
 
 if __name__ == "__main__":
     main()
