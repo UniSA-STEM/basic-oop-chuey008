ae97dcca69bd6f75bf8e072ff44780cb2f615b25
Me <Me@me>
2025-10-18 20:17:21 +1030

    # Testing in Main
    Errors appearing, fixing
    other files

--- Main.py
+++ Main.py
@@ -7,3 +7,19 @@ Username: <Chuey008>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
+from Asset import Asset, CryptoToken, DataSpike, RemovableDrive, SecurityChip, HardwarePatch
+from Rig import Rig
+from Hacker import Hacker
+
+def main():
+    # Create a hacker
+    hacker = Hacker("Fidelius")
+    print(hacker)
+
+    # Acquire a rig
+    print("\n-- Acquiring Rig --")
+    hacker.acquire_rig()
+    print(hacker)
+
+if __name__ == "__main__":
+    main()
